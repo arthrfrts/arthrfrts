f72436dfd8f11d78d5fcb38bc4ac86630166757a
Arthur Freitas <atpfreitas@me.com>
2026-03-21 12:07:08 -0300

    adds wander

---
+++ wander/wander.js
@@ -0,0 +1,28 @@
+const wander = {
+  // Other Wander consoles that visitors can reach from my console.
+  consoles: [
+    'https://susam.net/wander/',
+  ],
+
+  // My favourite websites and pages I recommend to the Wander community.
+  pages: [
+    'https://boingboing.net/',
+    'https://www.swiss-miss.com/',
+    'https://waxy.org/',
+    'https://le-chouchou.ghost.io/',
+  ],
+
+  // Websites and consoles to ignore.  When this console serves as
+  // your host console, it will never contact consoles or recommend
+  // web pages with addresses that match the following regular
+  // expression patterns.
+  ignore: [
+    // Off-topic since these are commercial services, not personal websites.
+    '.*://medium\\.com/.*',
+    '.*://.*\\.substack\\.com/.*',
+
+    // These do not load in the console due to frame-embedding restrictions.
+    '.*://cari\\.institute/.*',
+    '.*://wdl\\.mcdaniel\\.edu/.*',
+  ]
+}
